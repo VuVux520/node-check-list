dfa4a75ecb9f41526af6497b2c764a4e91218613
ADMIN\Admin <vycaoqer@gmail.com>
2021-04-12 21:57:57 +0700

    update blue_bird

--- blue_bird.js
+++ blue_bird.js
@@ -1,5 +1,6 @@
 var Promise = require('bluebird');
 const { get } = require('request');
+var readFile = Promise.promisify(require('fs').readFile);
 //new Promise(function(function resolve, function reject) resolver) -> Promise
 function getConnection(urlString){
     return new Promise(function(resolve){
@@ -49,6 +50,12 @@ function work(delay) {
 
 var promise = Promise.all([work(300), work(500), work(1000), work(2000)]);
 
+Promise.all([readFile('file.txt'),readFile('file2.txt')]).then((file) =>{
+    console.log(file[0].toString(), file[1].toString());
+    }).catch(function(err){
+        console.error('Cannot not read file ' + err.message);
+    });
+//
 promise.then((data) => {
     console.log('All done');
     data.forEach((text)=>{
@@ -85,3 +92,18 @@ Promise.join(getName(),getAge(),getSex(), function(name, age, sex){
 Promise.resolve([1,2,3]).get(-1).then((x)=>{
     console.log(x);
 })
+
+let step1 = () => {
+    new Promise((resolve, reject) => {
+        resolve(console.log('This is step 1'));
+    })
+}
+
+let step2 = () => {
+    new Promise((resolve, reject) => {
+        var things = 'This is something';
+        return Promise.map(things, (things)=>{
+
+        })
+    })
+}
